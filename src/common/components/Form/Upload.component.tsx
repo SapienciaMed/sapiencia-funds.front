78dd206ade7003f12a28e74c7d02d0abcc5c0cf4
victorgalvis <victorgalvis.85@gmail.com>
2023-10-10 13:15:10 -0500

    modificacion componente subir archivos

--- src/common/components/Form/Upload.component.tsx
+++ src/common/components/Form/Upload.component.tsx
@@ -1,54 +1,150 @@
-import React, { useRef } from "react";
-import { Control, Controller } from "react-hook-form";
+import React, { useRef, useState, useEffect, forwardRef, useImperativeHandle, useContext } from 'react';
 import { Toast } from 'primereact/toast';
+import { FileUpload, FileUploadHeaderTemplateOptions, FileUploadSelectEvent, FileUploadUploadEvent, ItemTemplateOptions } from 'primereact/fileupload';
+import { ProgressBar } from 'primereact/progressbar';
+import { Button } from 'primereact/button';
 import { Tooltip } from 'primereact/tooltip';
-import { FileUpload } from 'primereact/fileupload';
+import { Tag } from 'primereact/tag';
 
-interface IUploadComponent {
-  idInput: string;
-  control: Control<any>;
-  className?: string;
-  label?: string | React.JSX.Element;
-  classNameLabel?: string;
-  onFileSelect?: (e: any) => void;
-  onFileUpload?: (e: any) => void;
-  onFileRemove?: (file: any, callback: () => void) => void;
-  onFileClear?: () => void;
-  // ... otras propiedades específicas del componente FileUpload
+interface IFileUploadRef {
+    // Define las funciones que deseas exponer en la referencia
+    clearFiles: () => void;
+    // ... otras funciones personalizadas que puedas necesitar
 }
 
-export function UploadComponent({
-  idInput,
-  control,
-  className = "select-basic",
-  label,
-  classNameLabel = "text-main",
-  onFileSelect,
-  onFileUpload,
-  onFileRemove,
-  onFileClear,
-  // ... otras propiedades específicas del componente FileUpload
-}: IUploadComponent): React.JSX.Element {
-  const toast = useRef(null);
-  const fileUploadRef = useRef(null); // Agregar esta línea
-
-  return (
-    <div>
-      <Toast ref={toast}></Toast>
-
-      <Tooltip target=".custom-choose-btn" content="Choose" position="bottom" />
-      <Tooltip target=".custom-upload-btn" content="Upload" position="bottom" />
-      <Tooltip target=".custom-cancel-btn" content="Clear" position="bottom" />
-
-      <FileUpload
-        ref={fileUploadRef} // Agregar esta línea
-        name={idInput}
-        // Resto de las propiedades específicas del componente FileUpload
-        onUpload={onFileUpload}
-        onSelect={onFileSelect}
-        onError={onFileClear}
-        onClear={onFileClear}
-      />
-    </div>
-  );
+interface IProps {
+    // Define las propiedades que necesitas para tu componente
 }
+
+const FileUploadComponent = forwardRef<IFileUploadRef, IProps>((props, ref) => {
+    const toast = useRef<Toast>(null);
+    const fileUploadRef = useRef<FileUpload>(null);
+    const [totalSize, setTotalSize] = useState(0);
+
+    // Define las funciones de manejo de eventos, similar a tu componente de tabla
+    const onTemplateSelect = (e: FileUploadSelectEvent) => {
+       // ... implementa tu lógica aquí
+      let _totalSize = totalSize;
+      let files = e.files;
+
+      for (let i = 0; i < files.length; i++) {
+          _totalSize += files[i].size || 0;
+      }
+
+      setTotalSize(_totalSize);
+    };
+
+    const onTemplateUpload = (e: FileUploadUploadEvent) => {
+        // ... implementa tu lógica aquí
+        let _totalSize = 0;
+
+        e.files.forEach((file) => {
+            _totalSize += file.size || 0;
+        });
+
+        setTotalSize(_totalSize);
+        toast.current?.show({ severity: 'info', summary: 'Success', detail: 'File Uploaded' });
+    };
+
+    const onTemplateRemove = (file: File, callback: Function) => {
+        // ... implementa tu lógica aquí
+        setTotalSize(totalSize - file.size);
+        callback();
+    };
+
+    const onTemplateClear = () => {
+      setTotalSize(0);
+    };
+
+    const headerTemplate = (options: FileUploadHeaderTemplateOptions) => {
+        // ... implementa tu lógica aquí
+        const { className, chooseButton, uploadButton, cancelButton } = options;
+        const value = totalSize / 10000;
+        const formatedValue = fileUploadRef && fileUploadRef.current ? fileUploadRef.current.formatSize(totalSize) : '0 B';
+
+        return (
+            <div className={className} style={{ backgroundColor: 'transparent', display: 'flex', alignItems: 'center' }}>
+                {chooseButton}
+                {uploadButton}
+                {cancelButton}
+                <div className="flex align-items-center gap-3 ml-auto">
+                    <span>{formatedValue} / 1 MB</span>
+                    <ProgressBar value={value} showValue={false} style={{ width: '10rem', height: '12px' }}></ProgressBar>
+                </div>
+            </div>
+        );
+    };
+
+    const itemTemplate = (inFile: object, props: ItemTemplateOptions) => {
+      return (
+        <div className="flex align-items-center flex-column">
+            <i className="pi pi-image mt-3 p-5" style={{ fontSize: '5em', borderRadius: '50%', backgroundColor: 'var(--surface-b)', color: 'var(--surface-d)' }}></i>
+            <span style={{ fontSize: '1.2em', color: 'var(--text-color-secondary)' }} className="my-5">
+                Drag and Drop Image Here
+            </span>
+        </div>
+    );
+    };
+
+    const emptyTemplate = () => {
+        // ... implementa tu lógica aquí
+        return (
+          <div className="flex align-items-center flex-column">
+              <i className="pi pi-image mt-3 p-5" style={{ fontSize: '5em', borderRadius: '50%', backgroundColor: 'var(--surface-b)', color: 'var(--surface-d)' }}></i>
+              <span style={{ fontSize: '1.2em', color: 'var(--text-color-secondary)' }} className="my-5">
+                  Drag and Drop Image Here
+              </span>
+          </div>
+      );
+    };
+
+    const chooseOptions = { icon: 'pi pi-fw pi-images', iconOnly: true, className: 'custom-choose-btn p-button-rounded p-button-outlined' };
+    const uploadOptions = { icon: 'pi pi-fw pi-cloud-upload', iconOnly: true, className: 'custom-upload-btn p-button-success p-button-rounded p-button-outlined' };
+    const cancelOptions = { icon: 'pi pi-fw pi-times', iconOnly: true, className: 'custom-cancel-btn p-button-danger p-button-rounded p-button-outlined' };
+
+
+    // Define la función personalizada para limpiar archivos
+    const clearFiles = () => {
+      setTotalSize(0);
+    };
+
+    // Usa useEffect para inicializar el componente y configurar referencias
+    useEffect(() => {
+        if (ref) {
+            // Configura las funciones que deseas exponer en la referencia
+            useImperativeHandle(ref, () => ({
+                clearFiles: clearFiles,
+            }));
+        }
+    }, [ref]);
+
+    return (
+        <div>
+            <Toast ref={toast}></Toast>
+            <Tooltip target=".custom-choose-btn" content="Choose" position="bottom" />
+            <Tooltip target=".custom-upload-btn" content="Upload" position="bottom" />
+            <Tooltip target=".custom-cancel-btn" content="Clear" position="bottom" />
+
+            <FileUpload
+                ref={fileUploadRef}
+                name="demo[]"
+                url="/api/upload"
+                multiple
+                accept="image/*"
+                maxFileSize={1000000}
+                onUpload={onTemplateUpload}
+                onSelect={onTemplateSelect}
+                onError={onTemplateClear}
+                onClear={onTemplateClear}
+                headerTemplate={headerTemplate}
+                itemTemplate={itemTemplate}
+                emptyTemplate={emptyTemplate}
+                chooseOptions={chooseOptions}
+                uploadOptions={uploadOptions}
+                cancelOptions={cancelOptions}
+            />
+        </div>
+    );
+});
+
+export default React.memo(FileUploadComponent);
