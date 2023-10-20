5ca5576fb83942776e118a83a3c4c50df6a64e45
Carlos Benítez <carlosalgabez.08@gmail.com>
2023-10-20 15:58:39 -0500

    editar crear item

--- src/features/voting-results/hooks/item.create.hooks.tsx
+++ src/features/voting-results/hooks/item.create.hooks.tsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import useYupValidationResolver from "../../../common/hooks/form-validator.hook";
 import { createItems } from "../../../common/schemas/voting-schema";
-import { IItemCreate, IItemCreateRegTable, IVotingCreate } from "../../../common/interfaces/voting.interfaces";
+import { IItemCreate, IItemCreateRegTable, IItemCreateForm } from "../../../common/interfaces/voting.interfaces";
 import { EResponseCodes } from "../../../common/constants/api.enum";
 import { useVotingService } from "../../../common/hooks/voting-service.hook";
 import { useGenericListService } from "../../../common/hooks/generic-list-service.hook";
@@ -16,7 +16,7 @@ import { useVotingResults } from "./voting-create.hooks";
 
 
 export const useItemResults = (action, dataVoting) => {
-
+console.log("action ", action);
     const [sending, setSending] = useState(false);
     const { setMessage, authorization, setDataGrid, dataGrid } = useContext(AppContext);
 
@@ -25,7 +25,10 @@ export const useItemResults = (action, dataVoting) => {
     const [deparmetList, setDeparmentList] = useState([])
     const [typeProgram, setTypeProgram] = useState([]);
     const [programSelected, setProgramSelected] = useState();
+    const [activitySelected, setActivitySelected] = useState();
     const [activity, setActivity] = useState([]);
+    const [idItemEdit, setIdItemEdit] = useState(0);
+  
 
 
 
@@ -33,13 +36,27 @@ export const useItemResults = (action, dataVoting) => {
     const { createVoting } = useVotingService();
 
     const {
-        handleSubmit,
-        register,
-        formState: { errors },
-        reset,
-    } = useForm<IVotingCreate>({ resolver });
+      handleSubmit,
+      register,
+      formState: { errors },
+      reset,
+    } = useForm<IItemCreateForm>({
+      resolver,
+      defaultValues: {
+        porcentaje456: action == "edit" ? dataVoting.porcentaje456 : null,
+        porcentaje123: action == "edit" ? dataVoting.porcentaje123 : null,
+        totalCost: action == "edit" ? dataVoting.totalCost : null,
+        amount: action == "edit" ? dataVoting.amount : null,
+        activityValue: action == "edit" ? dataVoting.activityValue : null,
+        activity: action == "edit" ? dataVoting.idActivity : null,
+        program: action == "edit" ? dataVoting.idProgram : null,
+        productCode: action == "edit" ? dataVoting.productCode : null,
+        productCatalog: action == "edit" ? dataVoting.productCatalog : null,
+        directObject: action == "edit" ? dataVoting.directObject : null,
+      },
+    });
 
-
+console.log("dataVoting ", dataVoting);
     const { 
         getActivityProgram,
         getProgramTypes
@@ -63,38 +80,87 @@ export const useItemResults = (action, dataVoting) => {
 
     /*Functions*/
   const onSubmitCreateItem = handleSubmit((data: IItemCreateRegTable) => {
-      if (data) {
-                dataGrid.push({
-                  porcentaje456: data.porcentaje456,
-                  porcentaje123: data.porcentaje123,
-                  totalCost: data.totalCost,
-                  amount: data.amount,
-                  activityValue: data.activityValue,
-                  directObject: data.directObject,
-                  productCatalog: data.productCatalog,
-                  productCode: data.productCode,
-                  program: data.program,
-                  activity: data.activity,
-                  ident: Math.random(),
-                });
-                setMessage({
-                  OkTitle: "Aceptar",
-                  description:
-                    "Se ha agregado el item exitosamente",
-                  title: "Agregar Item",
-                  show: true,
-                  type: EResponseCodes.OK,
-                  background: true,
-                  onOk() {
-                    reset();
-                    setMessage({});
-                  },
-                  onClose() {
-                    reset();
-                    setMessage({});
-                  },
-                });
+    debugger
+      if (action == 'new') {
+        if (data) {
+          dataGrid.push({
+            porcentaje456: data.porcentaje456,
+            porcentaje123: data.porcentaje123,
+            totalCost: data.totalCost,
+            amount: data.amount,
+            activityValue: data.activityValue,
+            directObject: data.directObject,
+            productCatalog: data.productCatalog,
+            productCode: data.productCode,
+            program: data.program,
+            activity: data.activity,
+            ident: Math.random(),
+            idActivity: activitySelected,
+            idProgram:  programSelected
+          });
+          setMessage({
+            OkTitle: "Aceptar",
+            description: "Se ha agregado el item exitosamente",
+            title: "Agregar Item",
+            show: true,
+            type: EResponseCodes.OK,
+            background: true,
+            onOk() {
+              reset();
+              setMessage({});
+            },
+            onClose() {
+              reset();
+              setMessage({});
+            },
+          });
+        }
+    }
+    if (action == "edit") {
+      debugger;
+      if (dataGrid.find((obj) => obj.ident == idItemEdit)) {
+        (dataGrid.find((obj) => obj.ident == idItemEdit).porcentaje456 =
+          data.porcentaje456),
+          (dataGrid.find(
+            (obj) => obj.ident == idItemEdit
+          ).porcentaje123 = data.porcentaje123),
+          (dataGrid.find((obj) => obj.ident == idItemEdit).totalCost =
+            data.totalCost),
+          (dataGrid.find((obj) => obj.ident == idItemEdit).amount =
+            data.amount),
+          (dataGrid.find(
+            (obj) => obj.ident == idItemEdit
+          ).activityValue = data.activityValue),
+          (dataGrid.find(
+            (obj) => obj.ident == idItemEdit
+          ).directObject = data.directObject),
+          (dataGrid.find(
+            (obj) => obj.ident == idItemEdit
+          ).productCatalog = data.productCatalog),
+          (dataGrid.find((obj) => obj.ident == idItemEdit).productCode =
+            data.productCode),
+          (dataGrid.find((obj) => obj.ident == idItemEdit).program =
+            data.program),
+          (dataGrid.find((obj) => obj.ident == idItemEdit).activity =
+            data.activity);
+                  setMessage({
+                    OkTitle: "Aceptar",
+                    description: "Se ha editado el item exitosamente",
+                    title: "Editar Item",
+                    show: true,
+                    type: EResponseCodes.OK,
+                    background: true,
+                    onOk() {
+                      reset();
+                      setMessage({});
+                    },
+                    onClose() {
+                      reset();
+                      setMessage({});
+                    },
+                  });
       }
+    }
     });
 
     const confirmVotingCreation = async (data: IItemCreateRegTable) => {
@@ -171,7 +237,9 @@ export const useItemResults = (action, dataVoting) => {
                 setTypeProgram(programList);
             } else {
                 setTypeProgram([]);
-            }
+          }
+          
+          setIdItemEdit(action == 'edit' ? dataVoting.ident : 0);
             
         }
 
@@ -220,6 +288,10 @@ export const useItemResults = (action, dataVoting) => {
       setActivity,
       setDataGrid,
       dataGrid,
+      idItemEdit,
+      setIdItemEdit,
+      activitySelected,
+      setActivitySelected,
     };
 };
 
