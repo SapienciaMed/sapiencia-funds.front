09754aff79d401a2707328052bd22cc46a99a2a9
Carlos Holguin <cholguin@i4digital.com>
2023-12-05 17:51:43 -0500

    correcciones

--- src/features/control-report/pages/conditionalPages/consolidateTab.tsx
+++ src/features/control-report/pages/conditionalPages/consolidateTab.tsx
@@ -159,7 +159,7 @@ const ConsolidateTab = (data) => {
           {" "}
           <div className="container-sections-forms mt-24px p-0">
             <div
-              className="bold mt-24px mr-16px p-0"
+              className="bold mt-24px mr-16px mb-24px p-0"
               style={{ fontWeight: 500, fontSize: "29px", color: "#000000" }}
             >
               Totales
