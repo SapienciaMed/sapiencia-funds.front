c927d83679756b82596cf69510f621e2296e86f3
CarlosYepes <cmmontoya@i4digital.com>
2023-12-26 12:12:13 -0500

    feature security from legal Audit and %absorption

--- src/features/absorption-percentage/forms/consultAbsorptionPercentage/AbsorptionPercentageForm.tsx
+++ src/features/absorption-percentage/forms/consultAbsorptionPercentage/AbsorptionPercentageForm.tsx
@@ -29,6 +29,7 @@ const AbsorptionPercentageForm = ({
   setPaginateData,
   tableComponentRef,
   urlGet,
+  validateActionAccess,
   register,
 }) => (
   <div className="container-sections-forms mt-24px ml-16px mr-16px p-0">
@@ -61,23 +62,24 @@ const AbsorptionPercentageForm = ({
         </div>
       </div>
       <div className="button-save-container-display-percentage mr-24px">
-        {tableView && (
-          <div className="button-clean  mr-auto ml-25px">
-            <div>
-              <div
-                className={`title-button  ${
-                  width < 300 ? "font-medium" : "font-big"
-                } mt-12px mr-15px hover`}
-                onClick={handleAggItem}
-              >
-                <div className="button-border colorTittle alin pr-23px pl-23px">
-                  <BiPlusCircle />
-                  <span className="p-2px"> Agregar ítem </span>
+        {tableView &&
+          validateActionAccess("FONDOS_PORCENTAJE_ABSORCION_CREAR") && (
+            <div className="button-clean  mr-auto ml-25px">
+              <div>
+                <div
+                  className={`title-button  ${
+                    width < 300 ? "font-medium" : "font-big"
+                  } mt-12px mr-15px hover`}
+                  onClick={handleAggItem}
+                >
+                  <div className="button-border colorTittle alin pr-23px pl-23px">
+                    <BiPlusCircle />
+                    <span className="p-2px"> Agregar ítem </span>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        )}
+          )}
 
         {tableView && (
           <ButtonComponent
@@ -120,22 +122,23 @@ const AbsorptionPercentageForm = ({
             backgroundColor: "#e0e0e0",
           }}
         ></div>
-        {showFooterActions && (
-          <div className="button-save-container-display mt-20px mr-24px">
-            <ButtonComponent
-              value={
-                <>
-                  <div className="container-buttonText">
-                    <span>Descargar</span>
-                    <Svgs svg="excel" width={23.593} height={28.505} />
-                  </div>
-                </>
-              }
-              className="button-download large "
-              action={downloadCollection}
-            />
-          </div>
-        )}
+        {showFooterActions &&
+          validateActionAccess("FONDOS_PORCENTAJE_ABSORCION_XLSX") && (
+            <div className="button-save-container-display mt-20px mr-24px">
+              <ButtonComponent
+                value={
+                  <>
+                    <div className="container-buttonText">
+                      <span>Descargar</span>
+                      <Svgs svg="excel" width={23.593} height={28.505} />
+                    </div>
+                  </>
+                }
+                className="button-download large "
+                action={downloadCollection}
+              />
+            </div>
+          )}
       </>
     )}
   </div>
